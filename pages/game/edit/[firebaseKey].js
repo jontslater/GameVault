c82e3eb878e22ab9dfdc252ba56ac0a808e95a2c
jonathan slater <jonslater2005@gmail.com>
2024-03-19 19:02:55 -0500

    deplying

--- pages/game/edit/[firebaseKey].js
+++ pages/game/edit/[firebaseKey].js
@@ -1,9 +1,7 @@
-/* eslint-disable import/no-unresolved */
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-
-import GameForm from '../../../components/gameForm';
 import { getSingleGame } from '../../../api/games';
+import GameForm from '../../../components/GameForm';
 
 export default function EditBook() {
   const [editItem, setEditItem] = useState({});
@@ -14,6 +12,5 @@ export default function EditBook() {
     getSingleGame(firebaseKey).then(setEditItem);
   }, [firebaseKey]);
 
-  // TODO: pass object to form
   return (<GameForm obj={editItem} />);
 }
